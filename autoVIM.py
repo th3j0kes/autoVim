52de3ff3d6452fa79d54af401d08bbe057c36b01
Noe Ortega <noe.ortega@exodusintel.com>
2018-07-11 18:06:40 -0500

    added logic for downloading interface

--- autoVIM.py
+++ autoVIM.py
@@ -1,19 +1,26 @@
-from BeautifulSoup import BeautifulSoup
-import urllib2
 import subprocess
 import tempfile
 import shlex
 import os
+import argparse
 
-page = urllib2.urlopen("https://github.com/tpope/vim-fugitive")
-soup = BeautifulSoup(page)
-soup = soup.prettify()
-pwd = os.getcwd()
-os.chdir("/home/noeortega/.vim")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("interface", choices=["Pathogen", "VimPlug"])
+args = parser.parse_args()
+interface = args.interface
+
+#Currently only handles Pathogen install
+print("Interface: {}".format(interface))
+
+try:
+    os.chdir("/home/noeortega/.vim")
+except:
+    os.makedirs("~/.vim")
+    os.chdir("~/.vim")
+
 os.makedirs("bundle")
 os.makedirs("autoload")
-
-subprocess.Popeon("curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim")
 
 repos = [
     "https://github.com/tpope/vim-fugitive.git",
@@ -22,7 +29,21 @@ repos = [
     "https://github.com/vim-airline/vim-airline-themes.git",
     "https://github.com/ervandew/supertab.git"
 ]
+
 for repo in repos:
     os.chdir("/home/noeortega/.vim/testing")
     args = shlex.split("git clone " + repo)
     p = subprocess.Popen(args)
+
+
+def download_interface():
+    if interface == "Pathogen":
+        subprocess.Popen(
+            "curl -LSso ~/.vim/autoload/pathogen.vim \
+            https://tpo.pe/pathogen.vim"
+        )
+    elif interface == "VimPlug":
+        subprocess.Popen(
+            "curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+        )
