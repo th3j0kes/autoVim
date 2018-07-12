58f2b836c24dd4bd1ab265c1ec710ee08ee4e4d8
Noe Ortega <noe.ortega@exodusintel.com>
2018-07-12 18:06:20 -0500

    saving work

--- autoVIM.py
+++ autoVIM.py
@@ -10,40 +10,71 @@ parser.add_argument("interface", choices=["Pathogen", "VimPlug"])
 args = parser.parse_args()
 interface = args.interface
 
-#Currently only handles Pathogen install
+#Currently only handles Pathogen or VimPlug install
 print("Interface: {}".format(interface))
+#https://github.com/tpope/vim-pathogen
+#https://github.com/junegunn/vim-plug
 
 try:
-    os.chdir("/home/noeortega/.vim")
+    os.chdir("~/.vim")
 except:
     os.makedirs("~/.vim")
     os.chdir("~/.vim")
 
-os.makedirs("bundle")
-os.makedirs("autoload")
+#os.makedirs("bundle")
+#os.makedirs("autoload")
 
-repos = [
+#pathogen_vimrc = """execute pathogen#infect()
+#syntax on
+#filetype plugin indent on
+#""")
+
+#vimplug_vimrc = """plug#begin()
+#Plug '{}'
+#call plug#end
+#""".format(plugin)
+#********MUST RELOAD .VIMRC and :PlugInstall to install plugins*************
+
+def vimrc_config(interface):
+    if interface == "Pathogen":
+#        with open("/home/noeortega/.vimrc", "w") as vimrc:
+#            vimrc.write(pathogen_vimrc)
+
+vimrc_config(interface)
+
+
+def download_interface(interface):
+    if interface == "Pathogen":
+        print("[*]-----Downloading Pathogen-----[*]")
+        #subprocess.Popen(
+        #    "curl -LSso ~/.vim/autoload/pathogen.vim \
+        #    https://tpo.pe/pathogen.vim"
+        #)
+        print("[*]-----Download Complete-----[*]")
+    elif interface == "VimPlug":
+        print("[*]-----Downloading VimPlug-----[*]")
+        # subprocess.Popen(
+        #     "curl -fLo ~/.vim/autoload/plug.vim \
+        #     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+        # )
+        print("[*]-----Download Complete-----[*]")
+
+#download_interface(interface)
+
+#list of popular VIM plugins
+plugins = [
     "https://github.com/tpope/vim-fugitive.git",
     "https://github.com/scrooloose/nerdtree.git",
     "https://github.com/vim-airline/vim-airline.git",
     "https://github.com/vim-airline/vim-airline-themes.git",
-    "https://github.com/ervandew/supertab.git"
+    "https://github.com/ervandew/supertab.git",
+    "https://github.com/tpope/vim-commentary.git",
+    "https://github.com/SirVer/ultisnips.git",
+    "https://github.com/xuyuanp/nerdtree-git-plugin",
+    "https://github.com/ntpeters/vim-better-whitespace.git"
 ]
 
-for repo in repos:
-    os.chdir("/home/noeortega/.vim/testing")
-    args = shlex.split("git clone " + repo)
-    p = subprocess.Popen(args)
-
-
-def download_interface():
-    if interface == "Pathogen":
-        subprocess.Popen(
-            "curl -LSso ~/.vim/autoload/pathogen.vim \
-            https://tpo.pe/pathogen.vim"
-        )
-    elif interface == "VimPlug":
-        subprocess.Popen(
-            "curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-        )
+#for plugin in plugins:
+#    os.chdir("~/.vim/bundle")
+#    args = shlex.split("git clone " + repo)
+#    p = subprocess.Popen(args)
